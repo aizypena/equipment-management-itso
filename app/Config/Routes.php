c27cceb607d0f8702a36dff4f67a26f16efa94cb
aizyyy <jbpena101@gmail.com>
2024-11-11 10:55:20 +0800

    project creation

---
+++ app/Config/Routes.php
@@ -0,0 +1,18 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+// index route
+$routes->get('/', 'Index::index');
+
+// auth route
+
+// itso personnel route
+
+// associates route
+
+// students route
---
+++ app/Controllers/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controllers;
+
+class Index extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+}
+
+?>
